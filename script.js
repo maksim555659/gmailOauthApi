02037b035e7849895ab1de0d689a4320706af3d2
maksim555659 <555659@inbox.ru>
2021-02-26 00:25:31 +0300

    tets

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.onreadystatechange = function(){
             gapi.auth2.init({
                 client_id: '585715093898-32c8ahpkv4jmefeksqff4ev8eionufes.apps.googleusercontent.com'
             });
+            console.log('inited');
         });
     }
  }

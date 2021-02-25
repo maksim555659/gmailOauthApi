18ee34f735ce86c210b99fa483d823645a02a145
maksim555659 <555659@inbox.ru>
2021-02-25 23:49:47 +0300

    test

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-
 function init() {
   gapi.load('auth2', function() {
     /* Ready. Make a call to gapi.auth2.init or some other API */
     gapi.auth2.init(
         {
-            client_id: '585715093898-32c8ahpkv4jmefeksqff4ev8eionufes.apps.googleusercontent.com',
-            scope: 'https://www.googleapis.com/auth/userinfo.profile',
-            redirect_uri: 'https://maksim555659.github.io/index.html'
+            client_id: '585715093898-32c8ahpkv4jmefeksqff4ev8eionufes.apps.googleusercontent.com'
         }
     );
     console.log('inited');

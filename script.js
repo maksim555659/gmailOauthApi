d366f1897b3ccf06b22425ad09765e56bec2f67e
maksim555659 <555659@inbox.ru>
2021-02-26 00:01:52 +0300

    test

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-function init() {
-        gapi.load('auth2', function() {
-            /* Ready. Make a call to gapi.auth2.init or some other API */
-            gapi.auth2.init(
-                {
-                    client_id: '585715093898-32c8ahpkv4jmefeksqff4ev8eionufes.apps.googleusercontent.com'
-                }
-            );
-            console.log('inited');
-          });
-    }
+gapi.load('auth2', function () {
+    /* Ready. Make a call to gapi.auth2.init or some other API */
+    gapi.auth2.init({
+        client_id: '585715093898-32c8ahpkv4jmefeksqff4ev8eionufes.apps.googleusercontent.com'
+    });
+    console.log('inited');
+});

8f17acd4b0de6a68b39a8804a0c92fd31893e168
maksim555659 <555659@inbox.ru>
2021-02-26 00:16:01 +0300

    test

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 gapi.load('auth2', function () {
-    /* Ready. Make a call to gapi.auth2.init or some other API */
     gapi.auth2.init({
         client_id: '585715093898-32c8ahpkv4jmefeksqff4ev8eionufes.apps.googleusercontent.com'
     });
-    console.log('inited');
 });

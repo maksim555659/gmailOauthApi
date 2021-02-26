cc3c3171b36f5f05b0a1f95449b54d9ff490102f
maksim555659 <555659@inbox.ru>
2021-02-26 12:11:43 +0300

    test

--- script.js
+++ script.js
@@ -4,21 +4,22 @@ let authButton = document.getElementById('auth');
 
 authButton.onclick = auth;
 
-function auth(){
-    GoogleAuth.signIn()
-}
-
 function init(){
     if(document.readyState === 'complete'){
         gapi.load('auth2', function () {
             gapi.auth2.init({
-                client_id: '585715093898-32c8ahpkv4jmefeksqff4ev8eionufes.apps.googleusercontent.com'
+                client_id: '585715093898-32c8ahpkv4jmefeksqff4ev8eionufes.apps.googleusercontent.com',
+                scope: 'https://www.googleapis.com/auth/userinfo.profile'
             });
             console.log('inited');
         });
     }
     GoogleAuth.then(onInit, onError);
  }
+
+ function auth(){
+    GoogleAuth.signIn()
+}
 
  function onError(){
     alert('error!');
